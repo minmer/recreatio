66eaec8dfca9778f06adb130e0b4ac8a958e16b9
Michael Mleczek <michalmleczek@outlook.com>
2026-01-03 01:56:18 +0100

    Enabled Swagger

--- backend/Recreatio.Api/Program.cs
+++ backend/Recreatio.Api/Program.cs
@@ -61,11 +61,8 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("RecreatioWeb");
