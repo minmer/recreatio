4a51b6c777cd7fbba33e379151bb8c66087156ac
Michael Mleczek <michalmleczek@outlook.com>
2026-01-06 18:51:36 +0100

    Commit

--- backend/Recreatio.Api/obj/Debug/net8.0/Recreatio.Api.AssemblyInfo.cs
+++ backend/Recreatio.Api/obj/Debug/net8.0/Recreatio.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recreatio.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61cb1a5afc9c7c3d04acb8aa67147a3a5906171d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d169ba6bd66201582a6cd2ce191751c0ed5aaf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recreatio.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recreatio.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
